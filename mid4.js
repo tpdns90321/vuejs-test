a0f09480bb5d2b1260239c29ca86441920ca4ff8
tpdns90321 <tpdns9032100@gmail.com>
2020-05-04 09:30:37 +0900

    [test] mid4 완성

--- mid4.js
+++ mid4.js
@@ -1,3 +1,25 @@
-function sum() {
+function sum(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+
+	return sum;
 }
+
+function test_sum(callback) {
+	let arr = new Array();
+	for (let i = 0; i < 5; i++) {
+		arr[i] = Math.ceil(Math.random() * 10);
+	}
+
+	console.log(arr);
+	console.log(callback(arr));
+}
+
+test_sum(sum);
+test_sum(sum);
+test_sum(sum);
+test_sum(sum);
+test_sum(sum);
 
